b699d73f9fc8384fcf431529c4a1fa2894540215
CJ Bradshaw <coltonjbradshaw@gmail.com>
2021-06-09 23:36:55 -0600

    Beginning to add Logic
    -Number buttons now work.
    -Created function for operators to begin working.

--- calculator/wpfCalculator/MainWindow.xaml.cs
+++ calculator/wpfCalculator/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace wpfCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
         public MainWindow()
         {
+            //Start the Window
             InitializeComponent();
+            clearAll.Click += ClearAll_Click;
+            changeSign.Click += ChangeSign_Click;
+            percentage.Click += Percentage_Click;
+            calculate.Click += Calculate_Click;
+        
         }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Percentage_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber /= 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void ChangeSign_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber *= -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedNumber = int.Parse(((ContentControl)sender).Content.ToString());         
+
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedNumber}";
+            }
+            else
+            {
+                resultLabel.Content += $"+{selectedNumber}";
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+            }
+        }
+        private void ClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            if(resultLabel.Content.ToString() != "0")
+            {
+                resultLabel.Content = "0";
+            }
+        }
+
     }
 }
