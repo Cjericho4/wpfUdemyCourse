69033e65d8f6ea8c1e9990964f2bb3dc55cd0f1a
Colton Bradshaw <coltonjbradshaw@gmail.com>
2021-06-21 11:25:41 -0600

    Started Landmark AI
    -This course will help learn REST API and how to communicate with JSON and C#

---
+++ LandmarkAI/LandmarkAI/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace LandmarkAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+
+            dialog.Filter = " Image Files(*.jpg, *.png, *.tiff)|*.png;*.jpg;*.tiff|All Files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+            }
+
+            
+        }
+    }
+}
