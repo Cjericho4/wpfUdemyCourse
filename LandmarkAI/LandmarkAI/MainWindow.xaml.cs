8cb1b2b8a4e24d134f6c882eead0163655888a01
Colton Bradshaw <coltonjbradshaw@gmail.com>
2021-06-21 14:35:50 -0600

    Interact with API
    Added Newtonsoft for JSON deserialization.

--- LandmarkAI/LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/LandmarkAI/MainWindow.xaml.cs
@@ -2,6 +2,12 @@
 using System;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using LandmarkAI.Classes;
+using Newtonsoft.Json;
 
 namespace LandmarkAI
 {
@@ -25,9 +31,31 @@ namespace LandmarkAI
             {
                 string fileName = dialog.FileName;
                 selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                _ = MakePredictionAsync(fileName);
             }
 
             
+        }
+
+        private async static Task MakePredictionAsync(string fileName)
+        {
+            //Sets the Headers that are needed to interact with CustomVision API
+            string predictionURL = "https://westus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/a838f1ca-a059-4efa-ab3f-81c48667e65c/classify/iterations/Iteration1/image";
+            string predictionKey = "5f1e5a47184a4814b539efd72dfdbc3b";
+            string contentType = "application/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            //Set Http Client
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+
+            using var content = new ByteArrayContent(file);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+            var response = await client.PostAsync(predictionURL, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            List<Prediction> predictions = (JsonConvert.DeserializeObject<CustomVision>(responseString)).Predictions;
         }
     }
 }
